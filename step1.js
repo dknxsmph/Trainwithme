fc3c3a290296dfef72c6e2e3bd7f0c17aa351d96
nortorkor <immintchyy@gmail.com>
2021-08-17 22:51:32 +0700

    step1 again

--- step1.js
+++ step1.js
@@ -1,1 +1,19 @@
-console.log('Hello');
+console.log('Hello');       //console.log()เป็นคำสั่งในการแสดงผลคล้ายๆกับsout
+const Timeout = 1200;       //const เป็นตัวแปรที่เมื่อประกาศแล้วจะไม่สามารถre-declared หรือ updated ได้ต่ะ
+setTimeout(function() {         //เป็นตัวอย่างที่แสดงให้เห็นเกี่ยวกับSynchronos เมื่อถึงเวลาก็ต้องไปดูก่อนว่าstck ว่างอยู่หรือป่าว ถ้าว่างก็จะทำ ถ้าไม่ว่างก็ต้องรอก่อน
+    console.log(`Good bye`);
+},Timeout);
+var myName = "NATTHAKAN";
+console.log("MY NAME IS : " + myName +" "+ "THAWEEWATTHANAPRAYUN");
+const age = 20;
+console.log("AGE : " + age);
+var myName = `NORTORKOR`;
+console.log('MY NAME IS : ' + myName +' '+ 'THAWEEWATTHANAPRAYUN');
+let myMoney = 100000;
+console.log("I HAVE : " + myMoney +"\tBAHT");
+myMoney= 10;
+console.log("I HAVE : " + myMoney + "\tBAHT");
+if(age >= 20) {
+    console.log("You can drink alcohol")
+}else {
+    console.log("You should drink milk");}
