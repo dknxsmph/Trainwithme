6ae99e029845668d8ba8d6c800a1a5b8ecc2682e
nortorkor <immintchyy@gmail.com>
2021-08-31 14:46:48 +0700

    step4

---
+++ step4.js
@@ -0,0 +1,30 @@
+//Obj Examples
+//simple obj
+let NCT ={
+    Units : "127",
+    Name : "Ten"
+};
+//console.log(NCT);
+//Value is array
+let NCTUnits = {
+    Units : "127",
+    Member : ["Ten" , "Doyoung" , "Jeahyun"]
+};
+console.log(NCTUnits.Member[0])
+console.log(NCTUnits.Member[NCTUnits.Member.length -1])
+
+//console.log(NCTUnits);
+let sm = {
+    loca : 1485,
+    phone : 888888 ,
+    groups :{
+        NCT : "nice",
+        EXO : "very nice"
+    }
+};
+//console.log(sm);
+
+// for (key in NCTUnits){
+//     console.log(key)
+//     console.log(NCTUnits[key])
+// }
