2bb9f6d594854376651dcb0c4dcfd0e2601f28ab
nortorkor <immintchyy@gmail.com>
2021-09-07 12:39:20 +0700

    player

--- step9.js
+++ step9.js
@@ -11,17 +11,43 @@ class Player {
     //         this._lastName = "unknown";
     //     }
     // }
-    constructor() {
-
+    constructor(firstName = "unknown", lastName = "unknown") {
+        this._firstName = firstName;
+        this._lastName = lastName;
     }
     get name() { return this._name + " " + this._lastName; }
     get firstName() { return this._firstName; }
     get lastName() { return this._lastName; }
-    set firstName(name) { this._firstName = name; }
+    set firstName(fname) { this._firstName = fname; }
     set lastName(name) { this._lastName = name; }
-
 }
-let p1 = new Player();
-console.log(p1.firstName);
-let p2 = new Player();
-p1.firstName
+
+function player(firstName, lastName) {
+    console.log(`you are in player func`)
+    let fName;
+    let lName;
+    if (firstName) {
+        fName = firstName;
+    } else {
+        fName = "Unknown";
+    }
+    if (lastName) {
+        lName = "Unknown";
+    }
+    return `${fName},${lName}`;
+}
+
+function playerCon(firstName, lastName, sex) {
+    console.log(`you are in player Con`)
+    this._fName = firstName;
+    this._lName = lastName;
+    if (sex === 'F') {
+        this.title = `MS`
+    } else {
+        this.title = `MR`
+    }
+}
+let p1 = new playerCon(`N`, `T`, `F`);
+let p2 = new playerCon(`D`, `K`, `M`);
+console.log(p1);
+console.log(p2);
